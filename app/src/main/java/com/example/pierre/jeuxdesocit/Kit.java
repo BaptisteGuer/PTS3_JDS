78e117b6b4b80c35a39c002d81f2f35eb3fc53f7
i173442 <i173442@IUTLAVALAD.UNIV-LEMANS.FR>
2018-11-15 14:09:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pierre/jeuxdesocit/Kit.java
@@ -0,0 +1,14 @@
+package com.example.pierre.jeuxdesocit;
+
+public class Kit {
+
+    public String nom;
+    public Kit(String nom){
+        this.nom=nom;
+
+    }
+
+    public String getNom() {
+        return nom;
+    }
+}
---
+++ app/src/main/java/com/example/pierre/jeuxdesocit/ParamActivity.java
@@ -0,0 +1,19 @@
+package com.example.pierre.jeuxdesocit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ParamActivity extends AppCompatActivity {
+    public TextView nom;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.config);
+        nom = findViewById(R.id.textView);
+        nom.setText("" + MainActivity.getItemSelec());
+    }
+}
